f0f8d0263c479a67ed933e8a4ebb10ea8da21945
Michal Kaštan <41683416+BloodyBogan@users.noreply.github.com>
2020-05-22 17:09:45 +0200

    Basic functionality done

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@ const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 
 input.addEventListener('keyup', () => {
+  if (input.value === '') {
+    output.style.setProperty('--display', 'none');
+    output.style.setProperty('--cursor-animation', 'none');
+  } else {
+    output.style.setProperty('--display', 'inline-block');
+    output.style.setProperty(
+      '--cursor-animation',
+      'cursor 700ms ease-in-out infinite'
+    );
+  }
+
   output.innerText = input.value;
 });
